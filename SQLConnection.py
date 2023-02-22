5ead429a06b3ae43e8b6e7de80b47823f4ac24fa
Kim M Pambid <125942038+KMP086@users.noreply.github.com>
2023-02-22 17:23:08 +0800

    Add files via upload

--- SQLConnection.py
+++ SQLConnection.py
@@ -1,19 +1,26 @@
 #pip install pyodbc
 import pyodbc
 #s for single data or m for multiple data or n for no display
-def readsql(drive,servername,dbname, uname, pword, query, smn):
+def readsql(drive,servername,dbname, uname, pword, query, sm):
     conscript = "Driver=" + drive + ";Server=" + servername + ";Database=" + dbname + ";UID=" + uname + ";PWD=" + pword
-    print(conscript)
+    #print(conscript)
     cndb = pyodbc.connect(conscript)
     cursor = cndb.cursor()
     #stored proc query = 'exec sp_sproc(123, 'abc')'
     cursor.execute(query)
-    if smn != 'n':
-        for item in cursor:
-            if smn == 's':
-                return item[0]
-            elif smn == 'm':
-                return item
-    elif smn == 'n':
-        return "data has been altered"
+    for item in cursor:
+        if sm == 's':
+            return item[0]
+        elif sm == 'm':
+            return item
 
+#for insert into or update set
+def altersql(drive,servername,dbname, uname, pword, query, val):
+    #query = 'insert into table(a,b,c) value(?,?,?)'
+    #values = [(a,b,c)]
+    conscript = "Driver=" + drive + ";Server=" + servername + ";Database=" + dbname + ";UID=" + uname + ";PWD=" + pword
+    #print(conscript)
+    cndb = pyodbc.connect(conscript)
+    cursor = cndb.cursor()
+    cursor.execute(query, val)
+    cndb.commit()
