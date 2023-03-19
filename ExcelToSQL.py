e69b97da44e0d0c4298542a03a1fe8b359098ce5
Kim M Pambid <125942038+KMP086@users.noreply.github.com>
2023-03-19 08:13:29 +0800

    Update ExcelToSQL.py

--- ExcelToSQL.py
+++ ExcelToSQL.py
@@ -10,7 +10,7 @@ import re
 
 class approfile:
     #Setup Variables//////////////////////////////////////////////////////////////////////////////////////////
-    fpath = 'C:\\Users\\Name\\OneDrive - DSV\\Desktop\\folder\\folder\\'
+    fpath = 'C:\\Users\\Name\\OneDrive\\Desktop\\folder\\folder\\'
     hrf = 0 #row that will be set as column name
     sref = 2 #Starting df row/ reference row
     sqlcred = ('{SQL Server}', 'ServerName', 'Database', 'UserName', 'Password') # SQl Credentials
